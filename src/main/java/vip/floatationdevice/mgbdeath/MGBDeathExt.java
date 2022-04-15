2b911fbf82386244d80af422a54e58149349cc38
MCUmbrella <mcumbrella@protonmail.com>
2022-04-15 16:10:51 +0800

    Initial commit

---
+++ src/main/java/vip/floatationdevice/mgbdeath/MGBDeathExt.java
@@ -0,0 +1,31 @@
+package vip.floatationdevice.mgbdeath;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import static vip.floatationdevice.mgbridge.MGBridge.instance;
+
+public final class MGBDeathExt extends JavaPlugin implements Listener
+{
+
+    @Override
+    public void onEnable()
+    {
+        instance.getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("MGBridge can now forward player death messages");
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // nothing needed
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event)
+    {
+        instance.sendGuildedMsg("`" + event.getDeathMessage() + "`", null);
+    }
+}
